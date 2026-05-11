a74ad6afb18dc1a3fa2749ff12b8cf3622755c0b
sohaibs-liberary <muhammadsohaibshakeel18@gmail.com>
2026-05-11 16:25:05 +0500

    feat(count): Reorganize wordcounter to count package and update test data
    - Move Wordcounter.go from Wordcounter/ to count/ package directory
    - Migrate word.txt sample data file to count/ directory
    - Remove outdated word.txt from Wordcounter/ directory
    - Consolidate all counter functionality and test data in single package location

---
+++ count/Wordcounter.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var ch string
+
+func countWords(content string) int {
+
+	// Wrod Counting
+
+	count := 0
+	inWord := false
+
+	for _, ch := range content {
+		if ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' {
+			inWord = false
+		} else {
+			if !inWord {
+				count++
+				inWord = true
+			}
+		}
+	}
+	return count
+
+}
+
+// space counting
+
+func spaceCounting(constant string) int {
+
+	spaceCount := 0
+
+	for _, ch := range constant {
+
+		if ch == ' ' {
+			spaceCount++
+		}
+
+	}
+	return spaceCount
+
+}
+
+// Line Counting
+func Linecounter(content string) int {
+
+	Lineno := 0
+
+	for _, ch := range content {
+
+		if ch == '\n' {
+
+			Lineno++
+		}
+
+	}
+	return Lineno
+}
+
+// Letters
+func Letterscounter(constant string) int {
+	return len(constant)
+}
+
+//No Of  Special Characters
+
+func Spaceialchars(constant string) int {
+
+	Special := 0
+
+	for _, ch := range constant {
+		
+		switch ch {
+		case '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', '~', '`'  :
+			Special++
+		}
+
+	}
+	return Special
+
+}
+
+func main() {
+
+	start := time.Now()
+
+
+	fileData, _ := os.ReadFile("counter.txt")
+	content := string(fileData)
+
+	fmt.Println("Total Words  :", countWords(content))
+	fmt.Println("Total Spaces  :", spaceCounting(content))
+	fmt.Println("Total Lines  :", Linecounter(content))
+	fmt.Println("Total Letters :", Letterscounter(content))
+	fmt.Println("Total Special Characters :", Spaceialchars(content))
+
+	 fmt.Println("Execution time:",  time.Since(start))
+
+}
